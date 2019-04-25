92ba7631cfd11cc775ce7557575062183b18430b
pawel.gawin <pawel.gawin@PK4-8>
2019-04-25 09:46:40 +0200

    start obiect c#

---
+++ Cw_01/Cw_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person1 = new Human();
+            person1.name = "John";
+            person1.surname = "Kowalski";
+            Console.WriteLine(person1.name);
+
+            Human person2 = new Human();
+            person2.name = "Elizabeth";
+            person2.surname = person1.surname;
+        }
+        class Human
+        {
+            public string name, surname;
+            public int age;
+            public int height;
+        }
+    }
+}
